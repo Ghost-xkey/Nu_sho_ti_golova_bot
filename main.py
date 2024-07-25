07594592e735e7c5757eff3ab20959cd398d5878
Ghost-xkey <moskva2255@mail.ru>
2024-07-26 02:03:48 +0300

    Фикс 26.07

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-import logging
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.utils.executor import start_polling
+from aiogram.utils.markdown import hbold
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from config import TOKEN
 from handlers import register_handlers
 from admin import register_admin_handlers
 from utils import send_daily_message, send_yearly_message
 
+import logging
+
+# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-storage = MemoryStorage()
+# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
+storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+# Планировщик заданий
 scheduler = AsyncIOScheduler()
 
-# Register handlers
+# Регистрация обработчиков
 register_handlers(dp)
 register_admin_handlers(dp)
 
-# Schedule daily message
+# Настройка задач для планировщика
 scheduler.add_job(send_daily_message, "cron", hour=9, minute=0)
-
-# Schedule yearly message on 25th of July
 scheduler.add_job(send_yearly_message, "cron", month=7, day=25, hour=9, minute=0)
 
 async def on_startup(dispatcher):
