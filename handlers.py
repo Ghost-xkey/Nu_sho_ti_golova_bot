a28d1d8255dec68890f4fa344c781becd5466cdf
Ghost-xkey <moskva2255@mail.ru>
2024-07-25 08:25:08 +0300

    2

--- handlers.py
+++ handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router, types
-from aiogram.filters import Command, Text
+from aiogram.filters import Command, TextFilter
 from text import WELCOME_MESSAGE, HELP_MESSAGE
 from kb import main_keyboard
 
@@ -13,6 +13,6 @@ async def cmd_start(message: types.Message):
 async def cmd_help(message: types.Message):
     await message.answer(HELP_MESSAGE)
 
-@router.message(Text(equals="Привет", ignore_case=True))
+@router.message(TextFilter(equals="Привет", ignore_case=True))
 async def greet(message: types.Message):
     await message.answer("Привет! Как дела?")
