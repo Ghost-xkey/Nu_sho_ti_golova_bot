0604d9336df5662e0a14d5c60aec14c1fe5c500e
Ghost-xkey <moskva2255@mail.ru>
2024-07-25 08:43:03 +0300

    5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 from config import API_TOKEN
 from handlers import router
 from admin import admin_router
@@ -8,9 +9,11 @@ from db import create_tables
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация бота и диспетчера
+# Инициализация бота
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+
+# Инициализация диспетчера
+dp = Dispatcher()
 
 # Регистрация маршрутизаторов
 dp.include_router(router)
@@ -19,13 +22,14 @@ dp.include_router(admin_router)
 # Создание таблиц в БД
 create_tables()
 
-async def on_startup(dp: Dispatcher):
+async def on_startup():
     # Ваши действия при старте (например, уведомление админов)
     pass
 
-async def on_shutdown(dp: Dispatcher):
+async def on_shutdown():
     # Ваши действия при завершении (например, закрытие соединений)
     pass
 
+# Запуск бота
 if __name__ == '__main__':
-    dp.start_polling(bot, on_startup=on_startup, on_shutdown=on_shutdown, skip_updates=True)
+    dp.run_polling(bot, on_startup=on_startup, on_shutdown=on_shutdown)
