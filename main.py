678f92dac615690588c028478c1579028d593a8b
Ghost-xkey <moskva2255@mail.ru>
2024-07-26 02:16:33 +0300

    фикс 8

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram import start_polling
+from aiogram import start_polling  # Импорт start_polling напрямую из aiogram
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from config import TOKEN
-from handlers import router  # Импортируем router
+from handlers import router  # Импортируем router из handlers.py
 from utils import send_daily_message, send_yearly_message
 
 import logging
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-# Регистрация роутера
-dp.include_router(router)  # Включаем router в Dispatcher
+dp.include_router(router)  # Включение router в Dispatcher
 
-# Планировщик заданий
 scheduler = AsyncIOScheduler()
-
-# Настройка задач для планировщика
 scheduler.add_job(send_daily_message, "cron", hour=9, minute=0)
 scheduler.add_job(send_yearly_message, "cron", month=7, day=25, hour=9, minute=0)
 
