c8d114cab65286da7303b668f341c2ce9571642d
Ghost-xkey <moskva2255@mail.ru>
2024-07-25 08:41:33 +0300

    2

--- admin.py
+++ admin.py
@@ -1,7 +1,8 @@
-from aiogram import Router
+from aiogram import Router, types
+from aiogram.filters import Command
 
 admin_router = Router()
 
-@admin_router.message(Command('admin'))
+@admin_router.message(Command(commands=["admin"]))
 async def admin_command(message: types.Message):
     await message.answer("Привет, админ!")
