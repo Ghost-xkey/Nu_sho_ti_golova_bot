17065f722560be4c6f34f60615bb4097c4ccb8a0
Ghost-xkey <moskva2255@mail.ru>
2025-09-03 05:23:29 +0300

    Change database path to /app/data to avoid Docker volume conflicts
    - Changed DB_PATH from './bot_database.db' to '/app/data/bot_database.db'
    - This avoids conflicts with Docker volumes and file locking issues
    - The /app/data directory is mounted as a volume in docker-compose.yml

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = '7255987005:AAEMSy4B0zWvJcH5RoJas9o4pEYvPMA__0g'
-DB_PATH = './bot_database.db'  # Файловая база данных
+DB_PATH = '/app/data/bot_database.db'  # Файловая база данных в /app/data
 CHAT_ID = '-1002512455884'
 
 # Настройки времени отправки воспоминаний
