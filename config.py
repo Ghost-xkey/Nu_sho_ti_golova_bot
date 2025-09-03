14c73b659d13713a3b3d398ff38ce0a690410fa7
Ghost-xkey <moskva2255@mail.ru>
2025-09-03 05:29:49 +0300

    Change database path to /tmp to fix permission issues
    - Changed DB_PATH from '/app/data/bot_database.db' to '/tmp/bot_database.db'
    - /tmp directory always exists and has write permissions in Docker containers
    - This should fix the 'unable to open database file' error

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = '7255987005:AAEMSy4B0zWvJcH5RoJas9o4pEYvPMA__0g'
-DB_PATH = '/app/data/bot_database.db'  # Файловая база данных в /app/data
+DB_PATH = '/tmp/bot_database.db'  # Файловая база данных в /tmp
 CHAT_ID = '-1002512455884'
 
 # Настройки времени отправки воспоминаний
